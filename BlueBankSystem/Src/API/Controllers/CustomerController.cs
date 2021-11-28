ec9b13fe45268d7fab22ab92dbcf435a9508f134
Deidimila Fonseca <deidimila@gmail.com>
2021-11-27 21:54:41 -0300

    Create CustomerController

---
+++ BlueBankSystem/Src/API/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlueBank.System.Services.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        //APENAS PARA SIMULAR BANCO DE DADOS: TODO
+
+        private Dictionary<int, string> Customers { get; set; }
+
+        public CustomerController()
+        {
+            Customers = new Dictionary<int, string>();
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+    }
+}
--- BlueBankSystem/Src/API/Startup.cs
+++ BlueBankSystem/Src/API/Startup.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API
+namespace BlueBank.System.Services.API
 {
     public class Startup
     {
