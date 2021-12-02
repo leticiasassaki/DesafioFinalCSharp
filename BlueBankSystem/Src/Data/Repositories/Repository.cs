f588890b5b379d9a23b9f5f9ddb809076d320f9f
Deidimila Fonseca <deidimila@gmail.com>
2021-12-02 11:51:14 -0300

    AccountController

--- BlueBankSystem/Src/Data/Repositories/Repository.cs
+++ BlueBankSystem/Src/Data/Repositories/Repository.cs
@@ -10,44 +10,30 @@ namespace BlueBank.System.Data.Repositories
     public class Repository<TEntity>: IRepository<TEntity> where TEntity : Entity
     {
         private SystemContext _context;
-
         public Repository(SystemContext context)
         {
             _context = context;
         }
-
         public void Add(TEntity entity)
         {
             _context.Set<TEntity>().Add(entity);
             _context.SaveChanges();
         }
-
-
-
         public TEntity GetById(Guid id)
         {
             return _context.Set<TEntity>().Find(id);
-        }
-        
-        public void Update(TEntity customer)
+        }        
+        public void Update(TEntity entity)
         {
-            _context.Set<TEntity>().Update(customer);
+            _context.Set<TEntity>().Update(entity);
             _context.SaveChanges();
         }
-
-        /*public void Update(TEntity account)
-        {
-            _context.Set<TEntity>().Update(account);
-            _context.SaveChanges();
-
-        }*/
         public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate = null)
         {
             return predicate == null
                 ? _context.Set<TEntity>().AsQueryable()
                 : _context.Set<TEntity>().Where(predicate).AsQueryable();
         }
-
         public void Remove(Guid id)
         {
             throw new NotImplementedException();
--- BlueBankSystem/Src/Shared/Entities/Entity.cs
+++ BlueBankSystem/Src/Shared/Entities/Entity.cs
@@ -8,15 +8,12 @@ namespace BlueBank.System.Domain.Shared.Entities
 {
     public abstract class Entity
     {
-        public Guid Id { get; private set; }      
-
+        public Guid Id { get; private set; }
         public bool IsActive { get; set; }
-
         public Entity()
         {
             Id = Guid.NewGuid();
             IsActive = true;
-
         }
     }
 }
